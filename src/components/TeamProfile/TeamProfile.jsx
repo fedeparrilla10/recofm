606beff12b5b361a5185817848ea4d7e62f93d0e
fedeparrilla10 <federicoivanpd@gmail.com>
2023-07-29 20:19:40 +0300

    css general changes

--- src/components/TeamProfile/TeamProfile.jsx
+++ src/components/TeamProfile/TeamProfile.jsx
@@ -3,17 +3,29 @@ import './TeamProfile.css';
 const TeamProfile = ({ team }) => {
   return (
     <article className="profile">
-      <h2>{team.name}</h2>
-      <img
-        className="profile__logo"
-        src={team.logo}
-        alt={`Escudo de ${team.name}`}
-      />
-      <img
-        className="profile__stadium-img"
-        src={team.stadium.img}
-        alt={`Escudo de ${team.name}`}
-      />
+      <div className="profile__team">
+        <h2 className="profile__team__name">{team.name}</h2>
+        <img
+          className="profile__team__logo"
+          src={team.logo}
+          alt={`Escudo de ${team.name}`}
+        />
+      </div>
+
+      <div>
+        <h4 className="profile__stadium__name">{team.stadium.name}</h4>
+        <img
+          className="profile__stadium__img"
+          src={team.stadium.img}
+          alt={`Escudo de ${team.name}`}
+        />
+      </div>
+
+      <div>
+        <img className="profile__kits__img" src={team.kits.home} alt="" />
+        <img className="profile__kits__img" src={team.kits.away} alt="" />
+        <img className="profile__kits__img" src={team.kits.third} alt="" />
+      </div>
     </article>
   );
 };

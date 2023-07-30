37d5b5ae0aeabc1eb0292db93df8540fb5ba20b2
fedeparrilla10 <federicoivanpd@gmail.com>
2023-07-30 14:55:32 +0300

    add assets files + change footer content

--- src/core/Footer/Footer.jsx
+++ src/core/Footer/Footer.jsx
@@ -5,10 +5,6 @@ const Footer = () => {
     <>
       <footer className="footer">
         <h4 className="footer__developer">Desarrollado por Fede Parrilla</h4>
-        <p className="footer__statement">
-          El contenido de esta aplicación está basado en el videojuego Football
-          Manager, propiedad de Sports Interactive
-        </p>
       </footer>
     </>
   );

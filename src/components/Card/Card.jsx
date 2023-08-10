a334b5b09e702a321af18b57c0656446c26eb95f
fedeparrilla10 <federicoivanpd@gmail.com>
2023-08-10 18:46:11 +0300

    add react mq to cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,13 @@
+import { useMediaQuery } from 'react-responsive';
 import './Card.css';
 
 const Card = ({ title, content }) => {
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
+
   return (
     <div className="card">
       <h2 className="card__title">{title}</h2>
-      <p className="card__content">{content}</p>
+      {!isMobile && <p className="card__content">{content}</p>}
     </div>
   );
 };

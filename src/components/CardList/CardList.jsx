8bf066fa48a45a3d6434060d13c3f98880a5231f
fedeparrilla10 <federicoivanpd@gmail.com>
2023-08-10 18:55:57 +0300

    create const for cards content

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,28 +1,24 @@
 import { Link } from 'react-router-dom';
 import Card from '../Card/Card';
 import { FAST_SAVES, LONG_SAVES, LLM_SAVES } from '../../constants/filters';
+import {
+  FAST_SAVE_CONTENT,
+  LONG_SAVE_CONTENT,
+  LLM_SAVE_CONTENT,
+} from '../../constants/cards';
 import './CardList.css';
 
 const CardList = () => {
   return (
     <section className="cards">
       <Link to={`random_team/${FAST_SAVES}`}>
-        <Card
-          title="Partida Rápida"
-          content="Para aquellos que buscan una partida rápida, en la que asesinar el botón 'Continuar' sin tener que pararse a pensar si poner once tareas defensivas porque vas último y te toca enfrentar al City de Haaland de visitante."
-        />
+        <Card title="Partida Rápida" content={FAST_SAVE_CONTENT} />
       </Link>
       <Link to={`random_team/${LONG_SAVES}`}>
-        <Card
-          title="Partida Larga"
-          content="Para los que disponen de cierto tiempo libre para dedicarle al juego. Esta es, la forma más 'común' de jugar al Football Manager. Se requiere paciencia. Incluye 'sleeping giants' y clubes con potencial sin aprovechar."
-        />
+        <Card title="Partida Larga" content={LONG_SAVE_CONTENT} />
       </Link>
       <Link to={`random_team/${LLM_SAVES}`}>
-        <Card
-          title="Partida MLB"
-          content="Para aquellos a los que les gusta jugar de la forma más realista posible. Hay que saber sufrir, pero no hay nada más gratificante que ganar la Champions League después de haber empezado en el barro de la cuarta división danesa."
-        />
+        <Card title="Partida MLB" content={LLM_SAVE_CONTENT} />
       </Link>
     </section>
   );

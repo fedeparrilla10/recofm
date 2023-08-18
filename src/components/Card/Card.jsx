7fce696e0d775c56c31dd81018f9f88dde9c0f13
fedeparrilla10 <federicoivanpd@gmail.com>
2023-08-18 20:31:37 +0300

    add responsive css to cards and teamlist

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,18 @@
 import { useIsMobile } from '../../constants/mediaqueries';
 import './Card.css';
 
-const Card = ({ title, content, image, firstEx }) => {
+const Card = ({ title, content, image }) => {
   const isMobile = useIsMobile();
 
   return (
     <div className="card">
-      <img
-        className="card__img"
-        src={image}
-        alt={`Imagen representativa de una ${title}`}
-      />
+      {!isMobile && (
+        <img
+          className="card__img"
+          src={image}
+          alt={`Imagen representativa de ${title}`}
+        />
+      )}
       <h2 className="card__title">{title}</h2>
       {!isMobile && <p className="card__content">{content}</p>}
     </div>
